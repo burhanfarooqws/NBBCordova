60e1a867fc17e97887951725523e0ba41ce57ad7
Burhan <burhan@Burhans-Mac.local>
2017-02-20 14:21:08 +0500

    timer fix for UI logout

--- app/js/controllers/softtoken.js
+++ app/js/controllers/softtoken.js
@@ -1,4 +1,4 @@
-function SoftTokenCtrl($state, $scope, $rootScope, $cordovaDevice, DeviceService, $window, $cordovaDialogs, $cordovaLocalNotification) {
+function SoftTokenCtrl($state, $scope, $rootScope, $interval, $cordovaDevice, DeviceService, $window, $cordovaDialogs, $cordovaLocalNotification) {
     'ngInject';
 
     // ViewModel
@@ -17,8 +17,8 @@ function SoftTokenCtrl($state, $scope, $rootScope, $cordovaDevice, DeviceService
 
     $scope.init = function () {
         /*vm.deviceuuid = $cordovaDevice.getUUID();
-        window.alert(vm.deviceuuid);
-        $scope.$apply();*/
+         window.alert(vm.deviceuuid);
+         $scope.$apply();*/
         vm.showspinner = true;
         vm.regenerateSoftToken();
     };
@@ -48,37 +48,63 @@ function SoftTokenCtrl($state, $scope, $rootScope, $cordovaDevice, DeviceService
         });
     };
 
-    $rootScope.$on('$cordovaLocalNotification:trigger',
-        function (event, notification) {
-            if(notification.id == 1){
-                vm.generatedsofttoken = null;
-                $state.go('auth');
-            }
-        });
+    vm.IsTriggered = function() {
+        //alert('444');
+        /*$cordovaLocalNotification.isTriggered(1, function (present) {
+         // isScheduled() or isTriggered()
+         alert(present);
+         });*/
+        try {
+            $cordovaLocalNotification.isTriggered(1).then(function (isTriggered) {
+                //alert(isTriggered);
+                if(isTriggered == true){
+                    $interval.cancel(vm.IsTriggered);
+                    vm.generatedsofttoken = null;
+                    $state.go('auth');
+                }
+            });
+        }
+        catch(e){
+            //alert(e);
+        }
+    };
+
+    /*$rootScope.$on('$cordovaLocalNotification:trigger',
+     function (event, notification) {
+     if(notification.id == 1){
+     vm.generatedsofttoken = null;
+     $state.go('auth');
+     //$scope.$apply();
+     }
+     });*/
+
+    $interval(vm.IsTriggered, 2000);
 
     vm.regenerateSoftToken = function () {
         var generatesofttoken = $rootScope.generateSoftToken;
-         debugger; // eslint-disable-line
+        debugger; // eslint-disable-line
         vm.showspinner = true;
         DeviceService.generateSoftToken(generatesofttoken).then(function (data) {
-             debugger; // eslint-disable-line
+            debugger; // eslint-disable-line
             if (data != null && data.data.OTP) {
                 vm.generatedsofttoken = data.data.OTP;
                 vm.showspinner = false;
                 vm.show = true;
                 vm.setCodeLocalNotification(vm.generatedsofttoken);
                 vm.setCodeExpireyLocalNotification();
+
                 //$scope.$apply();
             }
             else {
                 vm.showspinner = false;
                 $cordovaDialogs.alert('you do not have any transaction which requires a security code, \nplease initiate a transaction before generating a security code', 'NBB').then(function () {
+                    $interval.cancel(vm.IsTriggered);
                     $state.go('auth');
                 });
             }
             $scope.$apply();
         }, function (error) {
-             debugger; // eslint-disable-line
+            debugger; // eslint-disable-line
             vm.showspinner = false;
             if (error.status == 400) {
                 var friendlyMessage = null;
@@ -95,10 +121,12 @@ function SoftTokenCtrl($state, $scope, $rootScope, $cordovaDevice, DeviceService
                     friendlyMessage = 'you do not have any transaction which requires a security code, \nplease initiate a transaction before generating a security code';
                 }
                 $cordovaDialogs.alert(friendlyMessage, 'NBB').then(function () {
+                    $interval.cancel(vm.IsTriggered);
                     $state.go('auth');
                 });
             } else {
                 $cordovaDialogs.alert('you do not have any transaction which requires a security code, \nplease initiate a transaction before generating a security code', 'NBB').then(function () {
+                    $interval.cancel(vm.IsTriggered);
                     $state.go('auth');
                 });
             }
@@ -110,19 +138,20 @@ function SoftTokenCtrl($state, $scope, $rootScope, $cordovaDevice, DeviceService
         $cordovaDialogs.confirm('Are you sure you want to delete?', 'NBB')
             .then(function (buttonIndex) {
                 // no button = 0, 'OK' = 1, 'Cancel' = 2
-                 debugger; // eslint-disable-line
+                debugger; // eslint-disable-line
                 if (buttonIndex == 1) {
                     vm.showspinner = true;
                     angular.isDefined($cordovaDevice.getDevice()); //unfortunately if the plugin is not installed calling this will cause fatal error
                     var deviceid = $cordovaDevice.getUUID();
                     DeviceService.deleteDevice(deviceid).then(function (data) {
-                         debugger; // eslint-disable-line
+                        debugger; // eslint-disable-line
                         if (data != null && data.data.IsExisting && data.data.IsDeleted) {
                             $cordovaDialogs.alert('Device deleted', 'NBB').then(function () {
                                 vm.showspinner = false;
                                 $cordovaLocalNotification.cancel([1, 2], function() {
                                     //alert("done");
                                 });
+                                $interval.cancel(vm.IsTriggered);
                                 $state.go('home');
                             });
                         }
@@ -133,7 +162,7 @@ function SoftTokenCtrl($state, $scope, $rootScope, $cordovaDevice, DeviceService
                         $scope.$apply();
                     }, function (error) {
                         //vm.Isdevicefound = false;
-                         debugger; // eslint-disable-line
+                        debugger; // eslint-disable-line
                         vm.showspinner = false;
                         $cordovaDialogs.alert('Device not found', 'NBB');
                         console.log('rejected');
